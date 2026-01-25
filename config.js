80742c41e5249c498a3e99b1c3fe9839698afa30
Chris Smith <chris@chameth.com>
2026-01-25 16:08:35 +0000

    Update the on-boarding config

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	platform: 'gitea',
+	platform: 'forgejo',
 	endpoint: 'http://forgejo.local:3000/api/v1/',
 	gitAuthor: 'Renovate Bot <git+renovate@chameth.com>',
 	username: 'renovate-bot',
@@ -7,6 +7,8 @@ module.exports = {
 	onboardingConfig: {
 		$schema: 'https://docs.renovatebot.com/renovate-schema.json',
 		extends: ['config:recommended'],
+		minimumReleaseAge: "28 days",
+		internalChecksFilter: "strict",
 	},
 	optimizeForDisabled: true,
 	persistRepoData: true,
