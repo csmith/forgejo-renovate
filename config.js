e3b8d71e4f13020587d3556dc6c01cdf68eacf95
Chris Smith <chris@chameth.com>
2026-08-25 14:40:17 +0100

    Maybe make private go modules work?

--- config.js
+++ config.js
@@ -1,14 +1,23 @@
 module.exports = {
-	platform: 'forgejo',
-	endpoint: 'https://git.yak-wall.ts.net/api/v1/',
-	gitAuthor: 'Renovate Bot <git+renovate@chameth.com>',
-	username: 'renovate-bot',
-	autodiscover: true,
-	onboardingConfig: {
-		$schema: 'https://docs.renovatebot.com/renovate-schema.json',
-		extends: ['local>meta/renovate'],
-	},
-	optimizeForDisabled: true,
-	persistRepoData: true,
-	allowedCommands: ['^.*$'],
+  platform: 'forgejo',
+  endpoint: 'https://git.yak-wall.ts.net/api/v1/',
+  gitAuthor: 'Renovate Bot <git+renovate@chameth.com>',
+  username: 'renovate-bot',
+  autodiscover: true,
+  onboardingConfig: {
+    $schema: 'https://docs.renovatebot.com/renovate-schema.json',
+    extends: ['local>meta/renovate'],
+  },
+  optimizeForDisabled: true,
+  persistRepoData: true,
+  allowedCommands: ['^.*$'],
+  hostRules: [
+    {
+      matchHost: 'git.yak-wall.ts.net',
+      token: process.env.RENOVATE_TOKEN,
+    },
+  ],
+  customEnvVariables: {
+    GOPRIVATE: 'chameth.com',
+  },
 };
